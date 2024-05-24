ccd6b60381a9312a8aae70352bf3121f2d30cd63
lisa <heloisamedeiros7@hotmail.com>
2024-05-23 23:03:06 -0300

    editar funcionando devidamente

--- components/Tabela.vue
+++ components/Tabela.vue
@@ -3,80 +3,94 @@
     const msgErro = useState('msgErro');
 
     function edicaoConcluida(id : number){
-        //separar por id
-        const novoCat = document.querySelector<HTMLInputElement>("#novoCat");
+        let novoCat = document.querySelectorAll<HTMLInputElement>("#novoCat");
         let verificador = false;
         const edicao = document.querySelectorAll(".edicao");
         const visualizacao = document.querySelectorAll(".visualizacao");
         const erro = document.querySelector("main")?.parentNode?.querySelector("#erro");
-        
-        if(typeof(novoCat?.value) == "string" && novoCat?.value.trim() != ""){
-            novoCat.value = novoCat.value.charAt(0).toUpperCase() + novoCat.value.slice(1).toLowerCase();
-            
-            for(let i = 0; i < categorias.length; i++){
-                if(categorias[i].categoria == novoCat.value){
-                    verificador = true;
-                    break;
-                };
-            };
-            
-            if(verificador){
-                msgErro.value = "Categoria já existente";
-                erro?.removeAttribute("hidden");
-            }
-            else{
-                for(var cat in categorias){
-                    if(categorias[cat].id == id){
-                        categorias[cat].categoria = novoCat.value;
-                        // fazer com q att automaticamente
-                        break;
-                    };
-                };
-                erro?.setAttribute("hidden", "hidden");
 
-                edicao.forEach(x => {
-                    x.setAttribute("hidden", "hidden");
+        for(var cat in categorias){
+            if(categorias[cat].id == id){
+                novoCat.forEach(x => {
+                    if(x.parentNode?.id == id){
+                        if(typeof(x.value) == "string" && x.value.trim() != ""){
+                            x.value = x.value.charAt(0).toUpperCase() + x.value.slice(1).toLowerCase();
+                            
+                            for(let i = 0; i < categorias.length; i++){
+                                if(categorias[i].categoria.includes(x.value)){
+                                    verificador = true;
+                                    break;
+                                }
+                            }
+
+                            if(verificador){
+                                // ELE NAO ESTA PEGANDO DIREITO, REFAÇA ESSA PARTE
+                                msgErro.value = "Categoria já existente";
+                                erro?.removeAttribute("hidden");
+                            }
+                            else{
+                                categorias[cat].categoria = x.value;
+                                erro?.setAttribute("hidden", "hidden");
+                                edicao.forEach(x => {
+                                    if(Number(x.id) == id){
+                                        console.log("coloca hidden no edicao")
+                                        x.setAttribute("hidden", "hidden");
+                                    }
+                                });
+                                
+                                visualizacao.forEach(x =>{
+                                    if(Number(x.id) == id){
+                                        console.log("tira hidden visualizacao")
+                                        x.removeAttribute("hidden");
+                                    }
+                                });
+                            }
+                        }
+                        else{
+                            msgErro.value = "O novo valor inserido é inválido. Tente novamente!";
+                            erro?.removeAttribute("hidden");
+                        };
+                    }
                 });
-                
-                visualizacao.forEach(x =>{
-                    x.removeAttribute("hidden");
-                });
-            };
+                break;
+            }
         }
-        else{
-            msgErro.value = "O novo valor inserido é inválido. Tente novamente!";
-            erro?.removeAttribute("hidden");
-        };
     }
 
-    function cancelarEdicao(){
-        //separar por id
+    function cancelarEdicao(id : Number){
         const edicao = document.querySelectorAll(".edicao");
         const visualizacao = document.querySelectorAll(".visualizacao");
         const erro = document.querySelector("main")?.parentNode?.querySelector("#erro");
 
         edicao.forEach(x => {
-            x.setAttribute("hidden", "hidden");
+            if(Number(x.id) == id){
+                x.setAttribute("hidden", "hidden");
+            }
         });
         
         visualizacao.forEach(x =>{
-            x.removeAttribute("hidden");
+            if(Number(x.id) == id){
+                x.removeAttribute("hidden");
+            }
         });
 
         erro?.setAttribute("hidden", "hidden");
     }
 
-    function editarCategoria(){
-        //separar por id
+    function editarCategoria(id : Number){
         const edicao = document.querySelectorAll(".edicao");
         const visualizacao = document.querySelectorAll(".visualizacao");
 
         edicao.forEach(x => {
-            x.removeAttribute("hidden");
+            if(Number(x.id) == id){
+                x.removeAttribute("hidden");
+            }
         });
 
         visualizacao.forEach(x =>{
-            x.setAttribute("hidden", "hidden");
+            if(Number(x.id) == id){
+                x.setAttribute("hidden", "hidden");
+            }
         });
     };
 
@@ -94,16 +108,16 @@
     <main style="margin: 15px 10%;">
         <table style="box-shadow: 10px 5px 5px #cacaca;">
             <thead style="border: #cacaca 1px solid;">
-                <th style="color: gray;">CATEGORIA</th>
+                <th colspan="6" style="color: gray;">CATEGORIA</th>
             </thead>
             <tbody>
                 <tr v-for="item in categorias">
-                    <td class="edicao" hidden><input id="novoCat" type="text" style="height: 30px; padding-left: 5px; border-radius: 5px; border: 2px #A8A8A8 solid;" :value="item.categoria"></td>
-                    <td class="edicao" hidden><button @click="edicaoConcluida(item.id)" style="border: none; background-color: #2451d4; color: white; width: 68px; border-radius: 5px; height: 30px;">Editar</button></td>
-                    <td class="edicao" hidden><button @click="cancelarEdicao" style="border: none; background-color: inherit; color: black; width: 68px;">Cancelar</button></td>
-                    <td class="visualizacao"><strong>{{ item.categoria }}</strong></td>
-                    <td class="visualizacao" @click="editarCategoria" style="cursor: pointer; color: blue;">Editar</td>
-                    <td class="visualizacao" @click="excluirCategoria(item.id)" style="cursor: pointer; color: red;">Excluir</td>
+                    <td :id="String(item.id)" class="edicao" hidden ><input id="novoCat" type="text" style="height: 30px; padding-left: 5px; border-radius: 5px; border: 2px #A8A8A8 solid;" :value="item.categoria"></td>
+                    <td :id="String(item.id)" class="edicao" hidden><button @click="edicaoConcluida(item.id)" style="border: none; background-color: #2451d4; color: white; width: 68px; border-radius: 5px; height: 30px;">Editar</button></td>
+                    <td :id="String(item.id)" class="edicao" hidden><button @click="cancelarEdicao(item.id)" style="border: none; background-color: inherit; color: black; width: 68px;">Cancelar</button></td>
+                    <td colspan="4" :id="String(item.id)" class="visualizacao"><strong>{{ item.categoria }}</strong></td>
+                    <td :id="String(item.id)" class="visualizacao" @click="editarCategoria(item.id)" style="cursor: pointer; color: blue;">Editar</td>
+                    <td :id="String(item.id)" class="visualizacao" @click="excluirCategoria(item.id)" style="cursor: pointer; color: red;">Excluir</td>
                 </tr>
             </tbody>
         </table>
